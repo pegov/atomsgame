77dbfa4e5e273b678d4c134f8d4d24054e2e3850
pegov <dvpegov@gmail.com>
2023-09-12 17:55:26 +0300

    add docker compose dev

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, _) => {
+    return {
+      ...config,
+      watchOptions: {
+        ...config.watchOptions,
+        poll: 800,
+        aggregateTimeout: 300,
+      },
+    }
+  }
 }
 
 module.exports = nextConfig
